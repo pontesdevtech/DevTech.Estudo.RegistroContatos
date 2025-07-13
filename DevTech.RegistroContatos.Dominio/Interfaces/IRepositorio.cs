fdda03ab4193f5404301450a6dd57f86e618b100
Tiago Pontes <pontesdevtech@outlook.com>
2025-07-12 23:50:40 -0300

    Criação da interface para repositórios

---
+++ DevTech.RegistroContatos.Dominio/Interfaces/IRepositorio.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq.Expressions;
+
+namespace DevTech.RegistroContatos.Dominio.Interfaces;
+
+public interface IRepositorio <T> where T : class
+{
+    Task<ICollection<T>> Listar();
+    Task<ICollection<T>> Listar(Expression<Func<T, bool>> condicao);
+    Task<T> BuscarPorId(Guid id);
+    Task<T> Salvar(T obj);
+    Task Excluir(T obj);
+}
