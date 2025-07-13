f42069b1b81e40142e0c3ac3694298bda4178fde
Tiago Pontes <pontesdevtech@outlook.com>
2025-07-13 03:37:08 -0300

    Ajustes nos serviços da camada de 'Domínio' para considerar nulos

--- DevTech.RegistroContatos.Dominio/Servicos/BuscarPorIdServico.cs
+++ DevTech.RegistroContatos.Dominio/Servicos/BuscarPorIdServico.cs
@@ -4,7 +4,7 @@ namespace DevTech.RegistroContatos.Dominio.Servicos;
 
 public static class BuscarPorIdServico<T> where T : class
 {
-    public static async Task<T> BuscarPorId(IRepositorio<T> repositorio, Guid id)
+    public static async Task<T?> BuscarPorId(IRepositorio<T> repositorio, Guid id)
     {
         return await repositorio.BuscarPorId(id);
     }
--- DevTech.RegistroContatos.Dominio/Servicos/ListarServicos.cs
+++ DevTech.RegistroContatos.Dominio/Servicos/ListarServicos.cs
@@ -5,12 +5,12 @@ namespace DevTech.RegistroContatos.Dominio.Servicos;
 
 public static class ListarServicos<T> where T : class 
 {
-    public static async Task<ICollection<T>> Listar(IRepositorio<T> repositorio)
+    public static async Task<ICollection<T>?> Listar(IRepositorio<T> repositorio)
     {
         return await repositorio.Listar();
     }
 
-    public static async Task<ICollection<T>> Listar(IRepositorio<T> repositorio, Expression<Func<T, bool>> condicao)
+    public static async Task<ICollection<T>?> Listar(IRepositorio<T> repositorio, Expression<Func<T, bool>> condicao)
     {
         return await repositorio.Listar(condicao);
     }
