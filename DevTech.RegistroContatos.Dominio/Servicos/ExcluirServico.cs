8052ec8da8851152f3e4fb4dfe2b9678790256f0
Tiago Pontes <pontesdevtech@outlook.com>
2025-07-13 04:49:45 -0300

    Ajustes nas classes de 'Serviço' da camada de 'Dominio'

--- DevTech.RegistroContatos.Dominio/Servicos/ExcluirServico.cs
+++ DevTech.RegistroContatos.Dominio/Servicos/ExcluirServico.cs
@@ -4,7 +4,7 @@ namespace DevTech.RegistroContatos.Dominio.Servicos;
 
 public static class ExcluirServico<T> where T : class
 {
-    public static async Task Excluir(IRepositorio<T> repositorio, T obj)
+    public static async Task Execute(IRepositorio<T> repositorio, T obj)
     {
         await repositorio.Excluir(obj);
     }
--- DevTech.RegistroContatos.Dominio/Servicos/SalvarServico.cs
+++ DevTech.RegistroContatos.Dominio/Servicos/SalvarServico.cs
@@ -4,7 +4,7 @@ namespace DevTech.RegistroContatos.Dominio.Servicos;
 
 public static class SalvarServico <T> where T : class
 {
-    public static async Task<T?> Salvar(IRepositorio<T> repositorio, T obj)
+    public static async Task<T?> Execute(IRepositorio<T> repositorio, T obj)
     {
         return await repositorio.Salvar(obj);
     }
