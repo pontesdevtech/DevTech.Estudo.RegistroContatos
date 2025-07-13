765b9b994728204d9fe7e31f02376cb2526cefb5
Tiago Pontes <pontesdevtech@outlook.com>
2025-07-13 03:09:52 -0300

    Ajustes na interface 'IRepositorio' para permitir retorno de objetos nulos

--- DevTech.RegistroContatos.Dominio/Interfaces/IRepositorio.cs
+++ DevTech.RegistroContatos.Dominio/Interfaces/IRepositorio.cs
@@ -4,9 +4,9 @@ namespace DevTech.RegistroContatos.Dominio.Interfaces;
 
 public interface IRepositorio <T> where T : class
 {
-    Task<ICollection<T>> Listar();
-    Task<ICollection<T>> Listar(Expression<Func<T, bool>> condicao);
-    Task<T> BuscarPorId(Guid id);
-    Task<T> Salvar(T obj);
+    Task<ICollection<T>?> Listar();
+    Task<ICollection<T>?> Listar(Expression<Func<T, bool>> condicao);
+    Task<T?> BuscarPorId(Guid id);
+    Task<T?> Salvar(T obj);
     Task Excluir(T obj);
 }
