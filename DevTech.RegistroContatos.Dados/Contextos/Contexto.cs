c107b90f075d9b9141effc15afdde17340b30d00
Tiago Pontes <pontesdevtech@outlook.com>
2025-07-13 03:26:08 -0300

    Criação das classes de contexto e de repositórios

---
+++ DevTech.RegistroContatos.Dados/Contextos/Contexto.cs
@@ -0,0 +1,12 @@
+﻿using DevTech.RegistroContatos.Dominio.Entidades;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevTech.RegistroContatos.Dados.Contextos;
+
+public class Contexto : DbContext
+{
+    public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+
+    public DbSet<Contato> Contatos { get; set; }
+    public DbSet<Pessoa> Pessoas { get; set; }
+}
---
+++ DevTech.RegistroContatos.Dados/Repositorios/ContatoRepositorio.cs
@@ -0,0 +1,50 @@
+﻿using DevTech.RegistroContatos.Dados.Contextos;
+using DevTech.RegistroContatos.Dominio.Entidades;
+using DevTech.RegistroContatos.Dominio.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace DevTech.RegistroContatos.Dados.Repositorios
+{
+    public class ContatoRepositorio(Contexto contexto) : IRepositorio<Contato>
+    {
+        private readonly Contexto _contexto = contexto;
+
+        public async Task<Contato?> BuscarPorId(Guid id)
+        {
+            return await _contexto.Contatos.FirstOrDefaultAsync(x => x.Id.Equals(id));
+        }
+
+        public async Task Excluir(Contato obj)
+        {
+            _contexto.Contatos.Remove(obj);
+            await _contexto.SaveChangesAsync();
+        }
+
+        public async Task<ICollection<Contato>?> Listar()
+        {
+            return await _contexto.Contatos.ToListAsync();
+        }
+
+        public async Task<ICollection<Contato>?> Listar(Expression<Func<Contato, bool>> condicao)
+        {
+            return await _contexto.Contatos.Where(condicao).ToListAsync();
+        }
+
+        public async Task<Contato?> Salvar(Contato obj)
+        {
+            if (BuscarPorId(obj.Id) is null)
+            {
+                _contexto.Contatos.Add(obj);
+            }
+            else
+            {
+                _contexto.Contatos.Update(obj);
+            }
+
+            await _contexto.SaveChangesAsync();
+            return obj;
+        }
+
+    }
+}
