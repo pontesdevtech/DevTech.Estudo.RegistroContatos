c2f717ff81e1935369030f7e05bba5fcb0fdc779
Tiago Pontes <pontesdevtech@outlook.com>
2025-07-15 00:22:07 -0300

    Configuração do 'Program.cs' para geração da documentação no Swagger

--- DevTech.RegistroContatos.Api/Program.cs
+++ DevTech.RegistroContatos.Api/Program.cs
@@ -1,13 +1,26 @@
+using DevTech.RegistroContatos.Api.Endpoints;
 using DevTech.RegistroContatos.Dados.Contextos;
+using DevTech.RegistroContatos.Dados.Repositorios;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// DbContext
 builder.Services.AddDbContext<Contexto>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("dev")));
+builder.Services.AddScoped<PessoaRepositorio>();
+
+// Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapPessoaEndpoints();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseHttpsRedirection();
 
 app.Run();
