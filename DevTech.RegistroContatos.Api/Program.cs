7bf3dcba86437e57fe337b4d2f97d67a10141334
Tiago Pontes <pontesdevtech@outlook.com>
2025-07-14 22:40:22 -0300

    Configuração da string de conexão e registro do contexto na api

--- DevTech.RegistroContatos.Api/Program.cs
+++ DevTech.RegistroContatos.Api/Program.cs
@@ -1,4 +1,11 @@
+using DevTech.RegistroContatos.Dados.Contextos;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<Contexto>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("dev")));
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
